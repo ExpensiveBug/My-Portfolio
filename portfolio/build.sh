988c83ecae45d83d8759e29dabc80bf813b9ac30
NidhiReddu <nidhireddu40@gmail.com>
2026-04-13 08:21:19 +0530

    removed migration

--- portfolio/build.sh
+++ portfolio/build.sh
@@ -2,15 +2,12 @@
 set -e
 
 echo "===> Installing Requirements..."
+python3 -m pip install -r requirements.txt
 
-pip install -r requirements.txt --break-system-packages
-
-echo "===> Running Migrations..."
-
-python3 manage.py migrate --noinput
+# echo "===> Running Migrations..."
+# python3 manage.py migrate --noinput
 
 echo "===> Collecting Static Files..."
-
 python3 manage.py collectstatic --noinput
 
 echo "===> Build Completed!"
